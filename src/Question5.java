292f663773d85ba4c9986f5271013bc6a6ff1d7c
1843096 <1843096@20.8.20.93>
2020-03-11 12:12:52 -0700

    Finished question 5

---
+++ src/Question5.java
@@ -0,0 +1,24 @@
+
+public class Question5 {
+	public static void main(String [] args) {
+		/*
+		 Consider the following code segment, which is intended to display 6.0
+		 double fact1 = 1 / 2;
+		 double fact2 = 3 * 4;
+		 double product = fact1 * fact2;
+		 System.out.println(product);
+		 */
+		
+		double fact1 = 1 / 2; //Although fact1 is a double, the divisor and the dividend are both integers. Therefore the result of the expression will be 0 as the decimal is truncated. This can be fixed by typecasting one of them as a double preventing integer division and preserving the decimal. 
+		double fact2 = 3 * 4;
+		double product = fact1 * fact2;
+		System.out.print("Incorrect Implementation Output: ");
+		System.out.println(product);
+		
+		double fact3 = (double) 1 / 2; //typecast shown
+		double fact4 = 3 * 4;
+		double product2 = fact3 * fact4;
+		System.out.print("Correct Implementation Output: ");
+		System.out.println(product2);
+	}
+}
