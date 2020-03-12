2f9073208abfc51212f6f433eca2aa5ee05d5759
1843096 <1843096@20.8.20.93>
2020-03-12 11:01:05 -0700

    U2Q2 complete

--- src/Unit2/Question2.java
+++ src/Unit2/Question2.java
@@ -21,6 +21,15 @@ public class Question2 {
 		Thing someThing1 = new Thing("Green"); //This calls one of the constructors of Thing, thus instantiating the object. Therefore, the object does not have a value of null.
 		
 		//Choice B
-		Thing someThing2 = null;
+		Thing someThing2 = new Thing(""); // This calls one of the constructors of Thing, thus instantiating the object. The Thing object will get have a String color of "". Despite it not containing anything meaningful, it will still have a data and therefore not be null.
+		
+		//Choice C
+		Thing someThing3 = new Thing(); // This calls the dault constructor of Thing, thus instantiating the object. This will causes the someThing3 to have data and therefore not be null.
+		
+		//Choice D (Correct)
+		Thing someThing4; // This code snippet will declare the someThing4 object, however it does not instantiate it. This will leave the object with a default value of null and will be the correct answer.
+		
+		//Choice E
+		//Thing("Green"); This code snippet makes no sense syntaxwise. You need to declare the object before you can instantiate it. The line itself is commented out as it creates a compiler error.
 	}
 }
