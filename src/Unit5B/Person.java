96dcefa6cb02522e9f83c460823bf651a63335c6
JaredJose <42820125+JaredJose@users.noreply.github.com>
2020-03-12 20:56:51 -0700

    created Person class to use in U5bQ11

---
+++ src/Unit5B/Person.java
@@ -0,0 +1,32 @@
+package Unit5B;
+
+//This class is utilized in question 11
+public class Person {
+	private String name;
+	private int feet;
+	private int inches;
+	
+	public Person(String nm, int ft, int in) {
+		name = nm;
+		feet = ft;
+		inches = in;
+	}
+	
+	public int heightInInches() {
+		return feet * 12 + inches;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String compareHeights(Person other) {
+		if (this.heightInInches() < other.heightInInches()) {
+			return name;
+		}
+		else if(this.heightInInches() > other.heightInInches()) {
+			return other.getName();
+		}
+		else return "Same";
+	}
+}
---
+++ src/Unit5B/Question11.java
@@ -0,0 +1,5 @@
+package Unit5B;
+
+public class Question11 {
+
+}
