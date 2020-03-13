f507a83ca0de8c0bb612f6092fe477fc24b8f655
JaredJose <42820125+JaredJose@users.noreply.github.com>
2020-03-12 20:41:42 -0700

    U5aQ14 finished

--- src/Unit5A/Question14.java
+++ src/Unit5A/Question14.java
@@ -1,5 +1,22 @@
 package Unit5A;
 
 public class Question14 {
-
+	public class Superhero{
+		private String name;
+		private String secretIdentity;
+		private int strength;
+		
+		public Superhero(String realName, String codeName) {
+			name = realName;
+			secretIdentity = codeName;
+			strength = 5;
+		}
+		
+		
+		//The correct modification to this code is to change the return type of levelUp() into void. This
+		//is warranted because levelUp is modifying a class variable, and thus not returning anything. 
+		public void levelUp(int amount) {
+			strength += amount;
+		}
+	}
 }
